eaea16ea2dc10d8dd418aa9a6c6c6098b98af634
Viral Ads Media <info.viraladsmedia@gmail.com>
2024-12-02 12:34:52 +0530

    SDFSDF

--- initialize.php
+++ initialize.php
@@ -4,7 +4,7 @@ if(!defined('base_url')) define('base_url','http://localhost/charity/');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"hikmat_123");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"hikmat_123");
 if(!defined('DB_NAME')) define('DB_NAME',"charity_db");
 ?>
